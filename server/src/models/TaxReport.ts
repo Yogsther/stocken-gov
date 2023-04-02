40d4bcd0a918d868b7bf283857d5203b1717b6d4
ChristofferBillman <christoffer.billman@gmail.com>
2023-04-02 15:00:28 +0200

    feat: changed schema for TaxReport. Uses Map<string,number> instead of ItemPickups

--- server/src/models/TaxReport.ts
+++ server/src/models/TaxReport.ts
@@ -1,17 +1,17 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose, { Model, Schema } from 'mongoose';
 
 // Removed id field because an id field is automatically added
 // by mongoose. "_id"
 export interface ITaxReport {
     player_guid: string
-    items: String
+    items: Map<string, number>
     date: number
     signed: boolean
 }
 
 const taxReportSchema: Schema = new Schema<ITaxReport>({
     player_guid: { type: String, required: true },
-    items: { type: [String], required: true },
+    items: { type: Map<string, number>, required: true },
     date: { type: Number, required: true },
     signed: { type: Boolean, required: true },
 })
