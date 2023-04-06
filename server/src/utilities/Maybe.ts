fd8d55166ccaeb5373c9ec3494957340a3c26a1e
ChristofferBillman <christoffer.billman@gmail.com>
2023-04-06 11:10:02 +0200

    feat: added functions isJust and isNothing

--- server/src/utilities/Maybe.ts
+++ server/src/utilities/Maybe.ts
@@ -1,3 +1,11 @@
-const Nothing = Symbol('nothing')
-type Nothing = typeof Nothing
-type Maybe<T> = T | Nothing
+export const Nothing = Symbol('nothing')
+export type Nothing = typeof Nothing
+export type Maybe<T> = T | Nothing
+
+export function isJust(monad: Maybe<any>): boolean  {
+    return monad != Nothing
+}
+
+export function isNothing(monad: Maybe<any>): boolean  {
+    return monad == Nothing
+}
