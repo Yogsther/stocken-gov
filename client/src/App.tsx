8c41eba4efed27bea08cd71177849bc73595d6ec
ChristofferBillman <christoffer.billman@gmail.com>
2023-04-24 18:47:04 +0200

    feat: added animation on page load.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 //import LoginPage from './pages/LoginPage/LoginPage'
 import TaxPage from './pages/TaxPage/TaxPage'
 
 import './App.css'
+import TransitionLifecycle from './components/TransitionLifecycle'
+import LoginPage from './pages/LoginPage/LoginPage'
 
 enum Pages {
   LOGIN,
@@ -10,17 +12,31 @@ enum Pages {
 }
 
 export default function App() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [currentPage, setCurrentPage] = useState<Pages>(Pages.LOGIN)
+  const [currentPage, setCurrentPage] = useState<Pages>(Pages.TAX)
+  const [renderPage, setRenderPage] = useState<boolean>(false)
 
-  //const onSignIn = () => setCurrentPage(Pages.TAX)
+  useEffect(() => setRenderPage(true), [])
+
+  const onSignIn = () => setCurrentPage(Pages.TAX)
 
   const getCurrentPage = (): JSX.Element => {
     switch(currentPage) {
-      case Pages.LOGIN: return /*<LoginPage onSignIn={onSignIn}/> */ <TaxPage/>
+      case Pages.LOGIN: return <LoginPage onSignIn={onSignIn}/>
       case Pages.TAX  : return <TaxPage/>
     }
   }
 
-  return getCurrentPage()
+  return (
+	<TransitionLifecycle
+		transition={{
+			initial:    { opacity: 0, transform: 'translateY(-25px)' },
+			transition: { opacity: 1, transform: 'translateY(0)' },
+			exit:       { opacity: 0, transform: 'translateY(25px)' },
+			duration: 500
+		}}
+		willRender={renderPage}
+	>
+		{getCurrentPage()}
+	</TransitionLifecycle>
+  )
 }
