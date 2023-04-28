7b3f69770eb4eeb964e646ddda6cf39305fdb8bd
ChristofferBillman <christoffer.billman@gmail.com>
2023-04-28 20:24:21 +0200

    docs: LoginPage

--- client/src/pages/LoginPage/LoginPage.tsx
+++ client/src/pages/LoginPage/LoginPage.tsx
@@ -3,6 +3,17 @@ import './LoginPage.css'
 import Icon from '../../assets/svgs/logo.svg'
 import { Pages, useNavigation } from '../../contexts/Navigator'
 
+/**
+ * Login-page on the site.
+ * 
+ *
+ * Example usage:
+ *     <LoginPage/>
+ *
+ * @author Christoffer Billman
+ * @version 1.0
+ * @since 2023-04-10
+ */
 export default function LoginPage() {
     
     const navigate = useNavigation()
