05fef1e3dd69bbfb39b7e60752c72ae7f50afeef
ChristofferBillman <christoffer.billman@gmail.com>
2023-04-28 20:18:51 +0200

    docs: Navigator component

--- client/src/contexts/Navigator.tsx
+++ client/src/contexts/Navigator.tsx
@@ -10,12 +10,27 @@ export enum Pages {
     NOT_IMPLEMENTED
 }
 
+// Context to store navigation state and setters in
 export const NavigationContextState = createContext(Pages.LOGIN)
 export const NavigationContextSetter = createContext<React.Dispatch<React.SetStateAction<Pages>>>(() => {})
 
+/**
+ * Provides a function that can be used to change what is currently displayed as the current page.
+ * @returns A function which navigates between the different pages.
+ */
 export const useNavigation = () => useContext(NavigationContextSetter)
 // MAYBE: Add hook for subscribing to navigationState, aka currentPage.
 
+/**
+ * Navigation provider for the entire website.
+ * 
+ * Example usage:
+ *     <Navigator/>
+ *
+ * @author Christoffer Billman
+ * @version 1.0
+ * @since 2023-04-28
+ */
 export default function Navigator() {
 
     const [currentPage, setCurrentPage] = useState(Pages.LOGIN)
