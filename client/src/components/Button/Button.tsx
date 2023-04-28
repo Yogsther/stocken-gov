03edc26a229a2e3c581dd209204a66714cab8781
ChristofferBillman <christoffer.billman@gmail.com>
2023-04-28 20:33:22 +0200

    docs: Button

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -8,6 +8,30 @@ interface ButtonProps {
     width?: string
 }
 
+/**
+ * A simple button.
+ * 
+ * Props:
+ *     text    - The text displayed inside the button.
+ *     onClick - Exectued on button click.
+ *     color   - Background color of the button. Default = 'black'
+ *     Icon    - Icon displayed to the left of text. Default: No icon displayed.
+ *     width   - The width of the button. Default = '100%'
+ * 
+ * Example usage:
+ *     <Button
+ *          text='Hello world!
+ *          onClick={() => console.log('Hello world!')}
+ *          color='black'
+ *          Icon={ExampleIconComponent}
+ *          width='100px'
+ *      />
+ *
+ * @author Christoffer Billman
+ * @version 1.0
+ * @since 2023-04-06
+ */
+
 export default function Button({text, onClick, color = 'black',width = '100%', Icon}: ButtonProps): JSX.Element {
     return (
         <button onClick={onClick} style={{width, backgroundColor: color}}> {Icon !== undefined && <Icon color='white'/>} {text}</button>
